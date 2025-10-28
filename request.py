57fbff9ce083f860b1945b216fd4d87e576b394c
Artyom <glotij17@gmail.com>
2025-10-28 23:08:23 +0300

    ge3

---
+++ request.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://artyom7777.pythonanywhere.com/ai"
+
+data = {
+    "message": "напиши Hello World!"
+}
+
+response = requests.post(url, json=data)
+
+if response.status_code == 200:
+    result = response.json()
+    print(result["response"])
+
+else:
+    print("ERROR:", response.json())
