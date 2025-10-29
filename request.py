12bd467dec9b7c687a8463e1ff439ecb69d0fd22
Artyom <glotij17@gmail.com>
2025-10-29 19:13:00 +0300

    ge3

--- request.py
+++ request.py
@@ -3,14 +3,19 @@ import requests
 url = "https://artyom7777.pythonanywhere.com/ai"
 
 data = {
-    "message": "напиши программу которая выведет ответ на 1 + (5 * 3 + 4) / 2"
+    "message": "напиши программу которая выведет ответ на 1 + 5 * 3 + 4 / 2"
 }
 
 response = requests.post(url, json=data)
 
-if response.status_code == 200:
-    result = response.json()
-    print(result["response"])
+now = 1
 
-else:
-    print("ERROR:", response.json())
+while response.status_code != 200:
+    print(f"ATTEMPT: {now}")
+
+    now += 1
+
+    response = requests.post(url, json=data)
+
+result = response.json()
+print(result["response"])
