4ecffe43b627b1894ebb8477ae1be104780d7ae8
artyom7774 <glotij17@gmail.com>
2025-10-29 09:41:24 +0300

    ge3

--- request.py
+++ request.py
@@ -3,7 +3,7 @@ import requests
 url = "https://artyom7777.pythonanywhere.com/ai"
 
 data = {
-    "message": "напиши программу которая пишет абракадабра каждые 5 кадров"
+    "message": "напиши программу которая выводит сумму 2 и 3 в консоль и что бы в консоль каждые 10 кадров писалось hello world"
 }
 
 response = requests.post(url, json=data)
